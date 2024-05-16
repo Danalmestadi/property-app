8b77d9c9f6ec60bd1317bd60df9e73d1c1693eff
danalmestadi <dana.almestadi@gmail.com>
2024-05-16 23:07:58 +0300

    design-update

--- src/Components/NavbarCp.jsx
+++ src/Components/NavbarCp.jsx
@@ -1,12 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
     return (
         <nav className="fixed bg-slate-50 border-b border-slate-200 w-full z-20 top-0 left-0 transition-all duration-300">
             <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
@@ -15,18 +9,7 @@ const Navbar = () => {
                     <span className="text-2xl font-bold whitespace-nowrap text-black">.</span>
                 </a>
 
-                <button
-                    className="md:hidden block text-black focus:outline-none"
-                    onClick={toggleMenu}
-                >
-                   <svg className="h-8 w-8 text-slate-900"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
-</svg>
-
-                  
-                </button>
-
-                <ul className={`md:flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ${isOpen ? 'block' : 'hidden'}`}>
+                <ul className="md:flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                     <li>
                         <a
                             href="/property"
